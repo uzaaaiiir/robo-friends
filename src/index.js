4e260388c147b93d2587d5d772297a5e675606e7
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-05-23 21:09:37 -0400

    Creating a Component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App";
+import App from "./App";
+import Hello from "./Hello";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
+        <Hello />
         <h1>Hello World</h1>
+        <App />
     </React.StrictMode>
 );
 
