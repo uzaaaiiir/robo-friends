2466e9b2c12130512b28ac5dfafe7a3f2c178617
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-05-24 21:14:19 -0400

    JSX

--- src/CardList.js
+++ src/CardList.js
@@ -1,19 +1,21 @@
 import React from "react";
 import Card from "./Card";
-import { robots } from "./robots";
 
-const CardList = function () {
-    console.log(robots);
-    const cardComponent = robots.map((user, i) => {
-        return (
-            <Card
-                id={robots[i].id}
-                name={robots[i].name}
-                email={robots[i].email}
-            />
-        );
-    });
-    return <div>{cardComponent}</div>;
+const CardList = ({ robots }) => {
+    return (
+        <div>
+            {robots.map((user, i) => {
+                return (
+                    <Card
+                        key={i}
+                        id={robots[i].id}
+                        name={robots[i].name}
+                        email={robots[i].email}
+                    />
+                );
+            })}
+        </div>
+    );
 };
 
 export default CardList;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import CardList from "./CardList";
 import "tachyons";
+import { robots } from "./robots";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <CardList />
+        <CardList robots={robots} />
     </React.StrictMode>
 );
 
