bc0e67195a8fb2060a62d0e10fb457cbd76f71df
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-05-24 20:13:41 -0400

    Props

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 const Card = function (props) {
+    console.log(props);
+    const { name, email, id } = props;
+    console.log(`${name}, ${email}, ${id}`);
     return (
         <div className="bg-light-green tc dib br3 pa3 ma2 grow bw2 shadow-5">
             <img
-                src={`https://robohash.org/${props.id}?200x200`}
+                src={`https://robohash.org/${id}?200x200`}
                 alt="Represents a Robot part of the Robo Friends Application."
             />
             <div>
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
